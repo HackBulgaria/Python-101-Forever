1fa18b7261e4eb5698bce6168b13d56d9c2ad9b1
Radoslav Georgiev <radorado@hacksoft.io>
2021-06-21 16:59:34 +0300

    Add solution file

--- C02-Linux-and-Python-Setup/05-C02P01/Solution01/solution.py
+++ C02-Linux-and-Python-Setup/05-C02P01/Solution01/solution.py
@@ -1,1 +1,30 @@
-# Coming soon
+# https://youtu.be/Ogm9lgW-nuw
+from collections import deque
+
+
+def reduce_file_path(path):
+    result = deque()
+
+    for i in path.split('/'):
+        if i == '..' and len(result) > 0:
+            result.pop()
+        if i not in ('.', '..', ''):
+            result.append(i)
+    return '/' + '/'.join(result)
+
+
+result = [
+    reduce_file_path(
+        '/home//user/courses/./Programming101-Python/week01/../'
+    ) == '/home/user/courses/Programming101-Python',
+    reduce_file_path('/') == '/',
+    reduce_file_path('/srv/../') == '/',
+    reduce_file_path('/srv/www/htdocs/wtf/') == '/srv/www/htdocs/wtf',
+    reduce_file_path('/srv/www/htdocs/wtf') == '/srv/www/htdocs/wtf',
+    reduce_file_path('/srv/./././././') == '/srv',
+    reduce_file_path('/etc//wtf/') == '/etc/wtf',
+    reduce_file_path('/etc/../etc/../etc/../') == '/',
+    reduce_file_path('//////////////') == '/',
+    reduce_file_path('/../../../../../../../') == '/'
+]
+print(result)
